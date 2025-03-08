ceb54fb6b3be2527d1c14219da14ecc9c10b6e32
Ivan Flores Flores <talivan51987@gmail.com>
2025-03-08 17:18:57 -0400

    subir el env

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // Middleware de manejo de errores
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
                   
